5cbeef8d9be532f607dc63f4e126197295b10915
root <root@T800.>
2024-11-16 16:06:42 +0000

    I have added Dual into __init__.py, this should be the reason why I had issues importing the package

---
+++ dual_autodiff/__init__.py
@@ -0,0 +1,3 @@
+from .dual import Dual  # Import Dual class from dual.py
+
+__all__ = ["Dual"]  # Defines what gets exported when using `from dual_autodiff import (any of the functions within class Dual)`
